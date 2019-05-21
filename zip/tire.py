21bb888ad7421df4182c51c65002a291a92aae1d
imm0nkey <yidingyidingli@gmail.com>
2019-05-21 15:31:11 +0800

    Delete tire.py

--- zip/tire.py
+++
@@ -1,9 +0,0 @@
-from math import pi
-
-def get_tire_diameter(dist_before_turn, dist_after_turn):
-    
-    # TODO - there's a bug in this function! Find and fix it!
-    
-    circumference = dist_after_turn - dist_before_turn
-    diameter = circumference * pi
-    return diameter
